7364282484d5cd57e5027a0be9aa648db8f8f237
anishbista92 <anishbista9235@gmail.com>
2024-10-24 20:18:35 +0545

    Initial commit

--- setup.py
+++ setup.py
@@ -6,4 +6,6 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=["Flask==3.0.3"],
+    author="Anish Bista",  # Your name
+    author_email="your.email@example.com",  # Your email
 )
